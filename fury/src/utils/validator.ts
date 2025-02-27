293dc7563877d7c30d79928f9df9b983166032b7
nyel-101 <dandevjr@gmail.com>
2025-02-27 00:20:18 +0000

    fix(core): decouple auth tokens

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b933cd4cee634ec88102d3e5ecfe0743 = () => {
-  return '2026-05-04T22:25:54Z';
+const node_bd72a90500a1489dbf8417c402c39be5 = () => {
+  return '2025-02-27T00:20:18Z';
 };
