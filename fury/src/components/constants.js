e541d5e590e373034fa6eac2517fdebcfc370cf1
nyel-101 <dandevjr@gmail.com>
2025-03-11 16:42:25 +0000

    perf(auth): fix session store

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6b5b186154a74a7381e840af18a63039 = () => {
-  return '2026-04-22T10:53:28Z';
+const node_6cac70b4faa14ab186b052266850fe4b = () => {
+  return '2025-03-11T16:42:25Z';
 };
