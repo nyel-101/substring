8884747a24cedb71b100bf0c308b27de37ef1498
nyel-101 <dandevjr@gmail.com>
2025-03-21 08:29:03 +0000

    perf(worker): update database index

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea7bc791aecb42048ae9acb99bdec615 = () => {
-  return '2025-03-08T20:45:34Z';
+const node_e5930b2dedf9495a8c2638ffb12d009f = () => {
+  return '2025-03-21T08:29:03Z';
 };
