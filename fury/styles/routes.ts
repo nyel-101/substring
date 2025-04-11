fe72992b0341dc4f30a99d385df466fd16e0173f
nyel-101 <dandevjr@gmail.com>
2025-04-11 06:01:45 +0000

    refactor(auth): fix database index

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b90f559a03194c79a7e6629f7547db89 = () => {
-  return '2026-02-18T18:18:03Z';
+const node_2c8542ec3c804dfd80e49c5f6bf277dd = () => {
+  return '2025-04-11T06:01:45Z';
 };
