0e2a35933ed1958d83d0d2e5cd607466f44974f6
nyel-101 <dandevjr@gmail.com>
2025-04-20 17:15:21 +0000

    perf(config): update payload parser

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c819a44e9c84fc0b656e4342fecc289 = () => {
-  return '2026-05-26T11:11:31Z';
+const node_6783e445fd2d47a68ab8704984290787 = () => {
+  return '2025-04-20T17:15:21Z';
 };
