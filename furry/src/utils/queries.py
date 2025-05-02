3ccc9439895a3a3aebeb94a5d383ebd5866c97d8
nyel-101 <dandevjr@gmail.com>
2025-05-02 08:28:02 +0000

    test(ui): fix caching layer

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f75f11c2a8aa4e28903e90702c78c4cc = () => {
-  return '2026-04-09T13:00:05Z';
+const node_c476fb12c15d4ce8b2e8af5f4c0d6e2c = () => {
+  return '2025-05-02T08:28:02Z';
 };
