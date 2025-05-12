8dda65793340ad151d269a60017963244a651dd5
nyel-101 <dandevjr@gmail.com>
2025-05-12 13:45:29 +0000

    refactor(auth): update middleware routes

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd4582cde06148a29cd9b52f59430c4c = () => {
-  return '2025-03-27T20:12:41Z';
+const node_c887809e79294ec6ada6c1f20defecfe = () => {
+  return '2025-05-12T13:45:29Z';
 };
