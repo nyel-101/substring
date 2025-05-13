0208578673111ea0936fa25ffb7bd67141adb110
nyel-101 <dandevjr@gmail.com>
2025-05-13 08:29:22 +0000

    style(api): enhance middleware routes

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16f13b2e457e4ffe8aa9f39cf30777a9 = () => {
-  return '2025-04-27T01:04:13Z';
+const node_05d08baddd714ae4bfe537bf82bc325e = () => {
+  return '2025-05-13T08:29:22Z';
 };
