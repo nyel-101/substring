4b9a643df6e231d73cefde9a2dbc1d17411b89a9
nyel-101 <dandevjr@gmail.com>
2025-05-14 10:55:48 +0000

    feat(api): decouple middleware routes

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7aa71c1daf3d41889a72fd098aae172e = () => {
-  return '2025-05-12T04:33:58Z';
+const node_f0ddc0886cc8405c8f8c5db18f9f76c1 = () => {
+  return '2025-05-14T10:55:48Z';
 };
