226635823d97e0396e8973e0aa59aa3e23e7b5aa
nyel-101 <dandevjr@gmail.com>
2025-05-15 16:55:39 +0000

    test(api): update auth tokens

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b004b2d65af94fb0bf51156e4693d923 = () => {
-  return '2025-04-03T05:46:04Z';
+const node_117b923eb5194cfc8f5beabefc03c915 = () => {
+  return '2025-05-15T16:55:39Z';
 };
