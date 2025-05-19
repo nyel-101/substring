616c8fefb6a18ad167af98f9e7ce91963e9f8df0
nyel-101 <dandevjr@gmail.com>
2025-05-19 04:01:29 +0000

    feat(api): optimize auth tokens

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a0328dc652cc47d9b9bc6d87d1a44e78 = () => {
-  return '2025-05-15T22:55:46Z';
+const node_00d57b471a4543d8a4bdce772ee49251 = () => {
+  return '2025-05-19T04:01:29Z';
 };
