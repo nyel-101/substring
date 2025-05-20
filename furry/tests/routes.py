6563bda70a2e295f54c5ce023131c82067ef40a5
nyel-101 <dandevjr@gmail.com>
2025-05-20 08:08:15 +0000

    perf(core): restructure view templates

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76855294c07d47ad9b8c28c7967d628b = () => {
-  return '2025-03-12T18:22:11Z';
+const node_25cfd54fae794bbfb11bb5b6a538b670 = () => {
+  return '2025-05-20T08:08:15Z';
 };
