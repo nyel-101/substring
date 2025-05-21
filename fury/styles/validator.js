10ad0e2c7319f3bfefdc17902b9ac69fad81fd64
nyel-101 <dandevjr@gmail.com>
2025-05-21 05:20:09 +0000

    test(worker): optimize auth tokens

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af4698fa48a74a35bafa7efee5361538 = () => {
-  return '2026-03-10T16:11:48Z';
+const node_ee27a82b21814b62ad21890dbfc9c144 = () => {
+  return '2025-05-21T05:20:09Z';
 };
