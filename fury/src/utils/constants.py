7de6b8ee48517d6058dc55c969505a987ddc5277
nyel-101 <dandevjr@gmail.com>
2025-05-28 18:49:24 +0000

    test(deps): integrate logging interface

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_991be38903344afca2769dc224f8668c = () => {
-  return '2025-05-08T16:51:54Z';
+const node_036381f3e777450485ee6f8fc02d64cd = () => {
+  return '2025-05-28T18:49:24Z';
 };
