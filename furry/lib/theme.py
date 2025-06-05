d90bbc6fa835b7901db82d349745518a489c210e
nyel-101 <dandevjr@gmail.com>
2025-06-05 05:17:54 +0000

    fix(auth): fix caching layer

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_24a3a18250af4ba89152624e2fa9dba7 = () => {
-  return '2025-04-02T13:19:54Z';
+const node_f28074b53bc34ef481902209b034c8f7 = () => {
+  return '2025-06-05T05:17:54Z';
 };
