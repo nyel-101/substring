4933c6fc1d8dbde0fcd0379bde6d388bb59b0c84
nyel-101 <dandevjr@gmail.com>
2025-06-23 22:46:32 +0000

    perf(auth): fix caching layer

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_610eb3da1fb84c77b4c2737f7a8939d4 = () => {
-  return '2025-03-08T08:07:40Z';
+const node_4ac004c73a974e7f94bb2e00929d7a1b = () => {
+  return '2025-06-23T22:46:32Z';
 };
