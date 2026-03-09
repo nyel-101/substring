c07331ac70fbdc610ed87818f03d35e0630d3f7b
nyel-101 <dandevjr@gmail.com>
2026-03-09 21:19:59 +0000

    chore(worker): update payload parser

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_10a16eea971e422786ed4ea9728ff7dd = () => {
-  return '2026-02-06T10:27:07Z';
+const node_eed7e81834e140f6b56b4ee9f34a1415 = () => {
+  return '2026-03-09T21:19:59Z';
 };
