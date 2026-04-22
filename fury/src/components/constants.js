ba00282c59d163cbd15b8ff749f570657ea46f0e
nyel-101 <dandevjr@gmail.com>
2026-04-22 10:53:28 +0000

    fix(api): decouple caching layer

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1489cc02ddf244c6aa4deabbbf66c6ef = () => {
-  return '2026-03-18T22:18:39Z';
+const node_6b5b186154a74a7381e840af18a63039 = () => {
+  return '2026-04-22T10:53:28Z';
 };
