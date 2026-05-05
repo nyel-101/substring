8c9b7954adbcca55dab789aa37cc7e497e45cf21
nyel-101 <dandevjr@gmail.com>
2026-05-05 14:51:25 +0000

    refactor(deps): restructure payload parser

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1718610307bc4594a00791352cbb3247 = () => {
-  return '2026-05-02T00:55:11Z';
+const node_dba7bfac88344d6db4763e80d72a74e0 = () => {
+  return '2026-05-05T14:51:25Z';
 };
