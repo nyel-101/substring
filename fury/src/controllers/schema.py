36b71a588219b6a8c52b9378ecdba71b1f471b17
nyel-101 <dandevjr@gmail.com>
2026-05-06 07:42:53 +0000

    fix(utils): enhance auth tokens

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a21f295915a042438a9be21bab7fd979 = () => {
-  return '2026-02-17T15:04:24Z';
+const node_242bb26aeb694ce3b1e401e171bcc984 = () => {
+  return '2026-05-06T07:42:53Z';
 };
