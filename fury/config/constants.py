2222710e8f0d7446ecab8064db94012b3c814afe
nyel-101 <dandevjr@gmail.com>
2026-05-21 14:56:21 +0000

    refactor(worker): fix database index

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb907e6768ef4b21ab8eeac4c64d8831 = () => {
-  return '2026-05-13T15:53:34Z';
+const node_a65d06a0297b484a82eaed07ba9bfa2f = () => {
+  return '2026-05-21T14:56:21Z';
 };
