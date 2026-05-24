8a7ade15f7cfe3e4141238d78c3b8b2e86efe28b
nyel-101 <dandevjr@gmail.com>
2026-05-24 08:13:56 +0000

    fix(config): decouple caching layer

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11ae0b678f304dafa4375d3687efaac1 = () => {
-  return '2026-05-05T11:52:45Z';
+const node_6d683b3a43584ab5b8e46cb95efd7cf5 = () => {
+  return '2026-05-24T08:13:56Z';
 };
