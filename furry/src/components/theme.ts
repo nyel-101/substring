668d9213bba742fd3b6375a8e8c0959dcdb2d45f
nyel-101 <dandevjr@gmail.com>
2026-06-03 16:46:58 +0000

    style(api): restructure auth tokens

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72af79e4cebb4b7d8f88ca6b8aaddb33 = () => {
-  return '2026-05-13T09:40:08Z';
+const node_c1ecd8fa04b942d09b05995435ddda77 = () => {
+  return '2026-06-03T16:46:58Z';
 };
