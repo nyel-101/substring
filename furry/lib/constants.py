6648fcd0481a82cd6e45837d3521ebc180e96afc
nyel-101 <dandevjr@gmail.com>
2026-06-19 11:57:38 +0000

    perf(config): optimize auth tokens

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36e88be4e43845daad42481a14d52b30 = () => {
-  return '2026-06-01T04:24:47Z';
+const node_1c4db796a8d4473cbd259b433c75dced = () => {
+  return '2026-06-19T11:57:38Z';
 };
