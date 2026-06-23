dd10c0a3f4f3bede4e1fc036fa6168bfdc1e3a56
nyel-101 <dandevjr@gmail.com>
2026-06-23 06:20:18 +0000

    perf(ui): fix session store

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a40a09c79e6a46829154d840acec1a2c = () => {
-  return '2026-02-27T06:44:30Z';
+const node_32a3b15861454f11abacb75ef520f0bd = () => {
+  return '2026-06-23T06:20:18Z';
 };
