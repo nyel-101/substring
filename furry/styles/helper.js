0667eb0c842efc60e70b6887ed2117c35bb77ad5
nyel-101 <dandevjr@gmail.com>
2026-06-23 22:05:48 +0000

    fix(auth): refactor payload parser

--- furry/styles/helper.js
+++ furry/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5fb874860914e66899db208d2eae3c6 = () => {
-  return '2026-04-07T02:20:36Z';
+const node_07cfd0ded62249e685a371d8c9573913 = () => {
+  return '2026-06-23T22:05:48Z';
 };
